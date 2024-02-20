ddc35d19f2c6876a8ff3948eee5b26acd3d24ccc
ncavchl <ncavchl@ajou.ac.kr>
2024-02-20 19:07:21 +0900

    update readme.md

--- src/containers/main/MainContainer.tsx
+++ src/containers/main/MainContainer.tsx
@@ -3,6 +3,8 @@ import { useEffect } from "react";
 import Script from "next/script";
 import BootPayTest from "@/components/bootPayTest/BootPayTest";
 import { useGetMemberInfoMutation } from "@/apis/sample/query";
+import { useQuery } from "@tanstack/react-query";
+import { axiosInstance } from "@/utils/axios";
 
 export default function MainContainer() {
   const { mutate } = useGetMemberInfoMutation();
@@ -22,8 +24,19 @@ export default function MainContainer() {
   };
 
   useEffect(() => {
-    console.log("window:::", window && window?.BootPay);
+    // console.log("window:::", window && window?.BootPay);
   }, []);
+
+  // const { isLoading, data } = useQuery(["testtest"], () => {
+  //   return axiosInstance
+  //     .get(
+  //       "/member?authCode=string&status=string&keyword=string&page=0&size=10&direction=ASC"
+  //     )
+  //     .then((res) => {
+  //       return res;
+  //     });
+  // });
+
   return (
     <>
       <Script
@@ -33,6 +46,7 @@ export default function MainContainer() {
       ></Script>
       <main>
         <div className="content-wrap">
+          {/** {JSON.stringify(data)}*/}
           <div className="content">
             <button onClick={handleClick}>멤버 정보 조회 api 테스트</button>
           </div>
